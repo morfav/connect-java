48e4b6acb54ad2cc8c9d35d3468f9a8fea8759b2
Richard Vowles <richard@bluetrainsoftware.com>
2022-06-22 18:38:15 +1200

    void functions returning 204 cause the ReturnStatus annotation to not function properly

--- libraries/openapi-jersey3-support/src/main/java/cd/connect/openapi/support/ReturnStatusContainerResponseFilter.java
+++ libraries/openapi-jersey3-support/src/main/java/cd/connect/openapi/support/ReturnStatusContainerResponseFilter.java
@@ -13,7 +13,8 @@ import java.lang.annotation.Annotation;
 public class ReturnStatusContainerResponseFilter implements ContainerResponseFilter {
   @Override
   public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
-    if (containerResponseContext.getStatus() == 200) {
+    // a 204 (which is void) from a POST actually returns a 204 (no content) and we need to intercept that as well
+    if (containerResponseContext.getStatus() == 200 || containerResponseContext.getStatus() == 204) {
       for (Annotation annotation : containerResponseContext.getEntityAnnotations()) {
         if(annotation instanceof ReturnStatus){
           containerResponseContext.setStatus(((ReturnStatus) annotation).code());
