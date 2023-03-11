e463db3deadbfe2c193d9225e4f397fe562fb028
Vlad <morfav@users.noreply.github.com>
2023-03-11 14:56:11 +1300

    File upload problem and potential hack

--- maven-plugins/openapi-jersey3-plugin/src/test/java/cd/connect/openapi/SampleRunner.java
+++ maven-plugins/openapi-jersey3-plugin/src/test/java/cd/connect/openapi/SampleRunner.java
@@ -1,11 +1,26 @@
 package cd.connect.openapi;
 
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.parser.core.models.ParseOptions;
 import org.junit.Test;
+import org.openapitools.codegen.ClientOptInput;
+import org.openapitools.codegen.DefaultGenerator;
 import org.openapitools.codegen.OpenAPIGenerator;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 
 public class SampleRunner {
+
+  private final Jersey3ApiGenerator codegen = new Jersey3ApiGenerator();
+
   @Test
   public void runGenerator() {
     String location = getClass().getResource("/sample2.yaml").getFile();
@@ -32,4 +47,28 @@ public class SampleRunner {
 //      .toArray(new String[0]));
 //  }
 
+  @Test
+  public void testMultipartForm() throws Exception {
+
+    String outputPath = "target/generated-test-sources/openapi/src/gen/java";
+    Files.createDirectories(Paths.get(outputPath));
+
+    OpenAPI openAPI = new OpenAPIParser()
+        .readLocation("upload.yaml", null, new ParseOptions()).getOpenAPI();
+
+    codegen.setOutputDir(outputPath);
+    codegen.additionalProperties().put("client", true);
+    codegen.additionalProperties().put("server", true);
+
+    ClientOptInput input = new ClientOptInput()
+        .openAPI(openAPI)
+        .config(codegen);
+
+    DefaultGenerator generator = new DefaultGenerator();
+    Map<String, File> files = generator.opts(input).generate().stream()
+        .collect(Collectors.toMap(File::getName, Function.identity()));
+
+    System.out.format("%s%n", files.get("FileServiceClient.java").getAbsolutePath());
+    System.out.format("%s%n", files.get("FileServiceServiceImpl.java").getAbsolutePath());
+  }
 }
